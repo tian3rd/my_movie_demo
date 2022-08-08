ebc1e29d705c772412687fdecbfb7aa949ee6fcc
tian <tianwu@outlook.com>
2022-08-08 21:19:41 +1000

    move poster data

---
+++ lib/data/posters.dart
@@ -0,0 +1,12 @@
+List<String> posters = [
+  // "https://image.tmdb.org/t/p/w780/sKvQUSyqsFq8e1ts6oo3Xp3dPH2.jpg",
+  // "https://image.tmdb.org/t/p/w780/93xA62uLd5CwMOAs37eQ7vPc1iV.jpg",
+  // "https://image.tmdb.org/t/p/w780/gF02RlJ9hHwqUrUTphEppsCrjmY.jpg",
+  // "https://image.tmdb.org/t/p/w780/k29qVmwWwR0i5A1UMIws1Vmv6hV.jpg",
+  // "https://image.tmdb.org/t/p/w780/kSlO1pHpwQfPQdgVPr7dJiJNtJ8.jpg",
+  "https://image.tmdb.org/t/p/original/sKvQUSyqsFq8e1ts6oo3Xp3dPH2.jpg",
+  "https://image.tmdb.org/t/p/original/93xA62uLd5CwMOAs37eQ7vPc1iV.jpg",
+  "https://image.tmdb.org/t/p/original/gF02RlJ9hHwqUrUTphEppsCrjmY.jpg",
+  "https://image.tmdb.org/t/p/original/k29qVmwWwR0i5A1UMIws1Vmv6hV.jpg",
+  "https://image.tmdb.org/t/p/original/kSlO1pHpwQfPQdgVPr7dJiJNtJ8.jpg",
+];
